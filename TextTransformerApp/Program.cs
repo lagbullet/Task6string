35a713f8be01b2ebb38c8f5a2784bf89428f2fbb
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-08-22 10:06:10 +0300

    1

---
+++ TextTransformerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTransformerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str="odc";
+            string str1 = null;
+            TextTransformer text = new TextTransformer();
+            for (int i = 0; i < 5; i++)
+            {
+                text.Add(str);
+                str += "b";
+            }
+            text.Add(str1);
+            text.Show();
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TextTransformerApp/TextTransformer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTransformerApp
+{
+    class TextTransformer
+    {
+        private List<string> Text = new List<string>();
+
+        public TextTransformer(string str)
+        {
+            Text.Add(str);
+        }
+
+        public TextTransformer() { }
+
+        public void Add(string item)
+        {
+            Text.Add(item);
+        }
+
+        public void Show()
+        {
+            foreach (string item in Text)
+                Console.WriteLine(item);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (string item in Text)
+            {
+                if (item != null)
+                    yield return item.ToUpper();
+                else
+                    yield return "This string was empty";
+            }
+        }
+    }
+}
